c1393ee3833fd0b25fbab056ff579ec6095f0776
Akash Suryawanshi <suryaakash7799@gmail.com>
2020-10-14 11:57:32 +0530

    0.0

---
+++ src/MyCode/Hash.java
@@ -0,0 +1,4 @@
+package MyCode;
+
+public class Hash {
+}
---
+++ src/MyCode/MyLinkedList.java
@@ -0,0 +1,19 @@
+package MyCode;
+
+public class MyLinkedList {
+     class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+
+        }
+     Node head;
+
+    }
+
+    }
+
+
