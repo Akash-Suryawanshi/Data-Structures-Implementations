670c89a2d30406106cc71c755a336584c3970d9a
Akash Suryawanshi <suryaakash7799@gmail.com>
2020-10-28 14:12:32 +0530

    Stashed changes
    0.1

---
+++ src/MyCode/AVL_Tree.java
@@ -0,0 +1,12 @@
+package MyCode;
+
+public class AVL_Tree extends SearchTree {
+    public void add_avl(int data) {
+        root = addavl(root, data);
+    }
+    public Node addavl(Node root, int data) {
+        if (root==null) {
+            Node n = new Node()
+        }
+    }
+}
