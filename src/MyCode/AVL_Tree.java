1f9f3bacc706d9c869ab132712d74675a546f51c
Akash Suryawanshi <suryaakash7799@gmail.com>
2020-10-29 19:30:22 +0530

    addavl, height corrected

--- src/MyCode/AVL_Tree.java
+++ src/MyCode/AVL_Tree.java
@@ -1,41 +1,60 @@
 package MyCode;
 
-
 public class AVL_Tree extends SearchTree {
     public void add_avl(int data) {
         root = addavl(root, data);
     }
+
+    public Node addavl(Node root, int data) {
+        if (root == null) {
+            Node cur = new Node(data);
+            return cur;
+        } else if (data < root.data) {
+            root.left = addavl(root.left, data);
+            root.left.parent = root;
+            if (root.right == null) {
+                root.height = root.left.height + 1;
+            } else {
+                root.height = Math.max(root.right.height, root.left.height) + 1;
+            }
+
+        } else {
+            root.right = addavl(root.right, data);
+            root.right.parent = root;
+            if (root.left == null) {
+                root.height = root.right.height + 1;
+            } else {
+                root.height = Math.max(root.right.height, root.left.height) + 1;
+            }
+        }
+        return root;
+    }
+
     public static boolean check_balance(Node root) {
-        if (root.left==null || root.right==null) {
+        if (root.left == null || root.right == null) {
             if (root.height > 2) return false;
-        }
-        else if (Math.abs(root.right.height - root.left.height) > 1) {
+        } else if (Math.abs(root.right.height - root.left.height) > 1) {
             return false;
+        } else {
+            check_balance(root.right);
+            check_balance(root.left);
         }
-        check_balance(root.right);
-        check_balance(root.left);
         return true;
     }
-    public Node addavl(Node root, int data) {
-        if (root==null) {
-            Node n = new Node(data);
-            return n;
-        }
-        insert(data);
-    }
+
     public static void main(String[] args) {
         AVL_Tree a = new AVL_Tree();
-        a.insert(5);
-        a.insert(2);
-        a.insert(7);
-        a.insert(8);
-        a.insert(1);
-        a.insert(9);
-        a.insert(10);
-        a.insert(6);
-        inorder(a.root);
-        check_balance(a.root);
+        a.add_avl(1);
+        a.add_avl(0);
+        a.add_avl(2);
+        System.out.println("root height = " + a.root.height);
+        System.out.println(check_balance(a.root));
+        System.out.println(a.root.right.height);
+        System.out.println(a.root.left.height);
+        System.out.println(a.root.height);
+
+        /*check_balance(a.root);*/
 
     }
-    }
+}
 
