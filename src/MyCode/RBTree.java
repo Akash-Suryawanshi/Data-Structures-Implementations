114208d8efed835ed09cc31bbd153b206b064d3e
Akash Suryawanshi <suryaakash7799@gmail.com>
2020-12-28 02:24:02 +0530

    Update RBTree.java

--- src/MyCode/RBTree.java
+++ src/MyCode/RBTree.java
@@ -1,4 +1,15 @@
 package MyCode;
 
 public class RBTree {
+    class Node{
+        int data;
+        Node left, right, parent;
+        boolean colour;
+        Node(int data) {
+            this.data = data;
+            this.left = this.right = this.parent = null;
+            this.colour = red;
+
+        }
+    }
 }
